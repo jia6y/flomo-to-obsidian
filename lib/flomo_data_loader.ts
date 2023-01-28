92647dceb195d744198147dadc066b18b58f59ae
OliyWork <nemo1023@gmail.com>
2023-01-28 20:16:42 +0800

    format code

--- lib/flomo_data_loader.ts
+++ lib/flomo_data_loader.ts
@@ -2,21 +2,27 @@ import *  as fs from 'fs';
 import { parse, HTMLElement } from 'node-html-parser';
 import { NodeHtmlMarkdown } from 'node-html-markdown';
 
+
+
 export class FlomoDataLoader {
     memoNodes: Array<HTMLElement>;
     tagNodes: Array<HTMLElement>;
     stat: Record<string, number>
+
     constructor() {
         this.memoNodes = [];
         this.tagNodes = [];
     }
+
     loadFlomoDataFrom(path: string): void {
         const flomoData = fs.readFileSync(path, "utf8");
         const root = parse(flomoData);
+
         this.memoNodes = root.querySelectorAll(".memo");
         this.tagNodes = root.getElementById("tag").querySelectorAll("option");
         this.stat = { "memo": this.memoNodes.length, "tag": this.tagNodes.length }
     }
+
     retrieveMemos(fn: (title: string, tsp_date: string, memo: string) => any): void {
         this.memoNodes.forEach(i => {
             const title = this.extrtactTitle(i.querySelector(".time").textContent);
@@ -25,16 +31,20 @@ export class FlomoDataLoader {
             fn(title, tspDate, mdContent)
         });
     }
+
     retrieveTags(fn: (tag: string) => any) {
         this.tagNodes.slice(1).forEach(i => {
             fn(i.textContent)
         })
     }
+
     private extrtactTitle(item: string): string {
         const re = /(-|:|\s)/gi;
         return item.replace(re, "_")
     }
+
     private extractContent(content: string): string {
         return NodeHtmlMarkdown.translate(content)
     }
+
 }
